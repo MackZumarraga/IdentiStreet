d44c4a0edd61b6227dedfbe0d130d0d342c41ff2
Mackinjosh <mackzumarraga@gmail.com>
2021-12-31 15:15:03 -0500

    added user and session controller, routes, users jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create, :show]
+    resource :session, only: [:create, :destroy]
+  end
 end
