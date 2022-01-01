53d56300a877785e87df7853121e03f8ddab815f
Mackinjosh <mackzumarraga@gmail.com>
2021-12-31 20:39:16 -0500

    added authroute for login and signup

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { Link, Route, HashRouter, Routes } from "react-router-dom";
-import { Provider } from "react";
+import { Link, Route, HashRouter, Redirect, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import HeaderContainer from "./header/header_container";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
+import { AuthRoute } from "../util/route_util";
 
 const App = () => (
     <div>
       <header>
-        <h1>IdentiStreet</h1>
-        <HeaderContainer/>
+        <Link to="/">
+            <h1>IdentiStreet</h1>
+        </Link>
       </header>
 
-      <Routes>
-            <Route path="/login" element={<LoginFormContainer/>}/>
-            <Route path="/signup" element={<SignupFormContainer/>} />
-      </Routes>
+      <Switch>
+            <AuthRoute exact path="/login" component={LoginFormContainer}/>
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+            <Route exact path="/" component={HeaderContainer}/>
+      </Switch>
     </div>
   );
   
