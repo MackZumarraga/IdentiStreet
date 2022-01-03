0738c5c9e7c7e32fb77b93bcfa1e5333af02959a
Mackinjosh <mackzumarraga@gmail.com>
2022-01-03 11:55:05 -0500

    added setup for heroku

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,6 +8,7 @@ import HeaderContainer from "./header/header_container";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
 import ListingsIndexContainer from "./listings/listing_index_container"
+import FooterContainer from "./footer/footer_container";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 const App = () => (
@@ -15,6 +16,7 @@ const App = () => (
       <Route path="/" component={HeaderContainer}/>
       <Route path="/" component={NavBar}/>
       <Route exact path="/" component={Home}/>
+      <Route path="/" component={FooterContainer}/>
       <Route path="/search/:filter" component={ListingsIndexContainer}/>
 
       <Switch>
