2e471fb02009f5304bda4f684750772afd6bff57
Mackinjosh <mackzumarraga@gmail.com>
2022-01-02 19:35:55 -0500

    designed navbar search

--- frontend/components/nav_bar/nav_bar_search.jsx
+++ frontend/components/nav_bar/nav_bar_search.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default () => (
 
-    <div>
-        <form>
-          <input type="text" placeholder='e.g. address, building, agent'></input>
-          <button>O</button>
+    <div className="nav-search-container">
+        <form className="search-form">
+          <input type="text" placeholder='e.g. address, building, agent' className="search-bar-input"></input>
+          <button className="search-button">
+              <span className="search-button-space">Search</span>
+          </button>
         </form>
     </div>
 
