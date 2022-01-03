afd0924c23c7bbe7312d5c7b562df8bfe2c1f60a
Mackinjosh <mackzumarraga@gmail.com>
2022-01-02 23:28:16 -0500

    Edited nav bar css

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -10,8 +10,6 @@ const sessionReducer = (state = _nullSession, action) => {
     
     switch(action.type) {
         case RECEIVE_CURRENT_USER:    
-            // nextState['id'] = action.currentUser.id
-            // return nextState
             return { id: action.currentUser.id };
         case LOGOUT_CURRENT_USER:
             return _nullSession;
