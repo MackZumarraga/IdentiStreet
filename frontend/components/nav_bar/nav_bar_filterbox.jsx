41603a869e3db63a7a417e84712e9c003597fba2
Mackinjosh <mackzumarraga@gmail.com>
2022-01-04 00:40:33 -0500

    edited more of filterbox css

--- frontend/components/nav_bar/nav_bar_filterbox.jsx
+++ frontend/components/nav_bar/nav_bar_filterbox.jsx
@@ -10,19 +10,28 @@ export default () => (
             <div className="filter-box-type-title">Rentals</div>
             <form className="filter-box-form">
                 <div className="classic-search">
-                    <label className="filter-box-label">LOCATION
-                        <input type="text" placeholder='Manhattan'></input>
+                    <label className="filter-box-label">
+                        <div className="label-title">LOCATION</div>
+                        <input type="text" placeholder='Manhattan' className="filter-box-input"></input>
                     </label>
-                    <label className="filter-box-label">MINIMUM PRICE
-                        <input type="text" placeholder='Any' className="filter-box-input"></input>
+                    <label className="filter-box-range">
+                        <div>
+                            <div className="label-title">MINIMUM PRICE</div>
+                            <input type="text" placeholder='Any' className="filter-box-input-range"></input>
+                        </div>
+                        &nbsp;
+                        <div>
+                            <div className="label-title">MAXIMUM PRICE</div>
+                            <input type="text" placeholder='Any' className="filter-box-input-range"></input>
+                        </div>
                     </label>
-                    <label className="filter-box-label">MAXIMUM PRICE
-                        <input type="text" placeholder='Any' className="filter-box-input"></input>
-                    </label>
-                    <label className="filter-box-label">BEDROOMS
+                    <label className="filter-box-label">
+                        <div className="label-title">BEDROOMS</div>
                         <input type="text" placeholder='Studio 1 2 3 4+' className="filter-box-input"></input>
                     </label>
-                    <button>SEARCH</button>
+                    <label className="filter-box-label">
+                        <button className="filter-search-button">SEARCH</button>
+                    </label>
                 </div>
                 <AdvancedSearch/>
             </form>
