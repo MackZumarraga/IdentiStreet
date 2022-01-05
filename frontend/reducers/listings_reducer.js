7b3114b0758f5b97e1543ba6826a500a742cb63e
Mackinjosh <mackzumarraga@gmail.com>
2022-01-04 23:50:53 -0500

    added listings reducer

---
+++ frontend/reducers/listings_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_LISTINGS, RECEIVE_LISTING, REMOVE_LISTING, RECEIVE_ERRORS } from "../actions/listing_actions";
+
+const listingsReducer = (state = {}, action) => {
+    Object.freeze(state)
+    const nextState = Object.assign({}, state);
+
+    switch(action.type) {
+      case RECEIVE_LISTINGS:
+        return action.listings;
+      case RECEIVE_LISTING:
+        const newListing = { [action.listing.id]: action.listing };
+        return Object.assign({}, state, newListing);
+      case REMOVE_LISTING:
+        delete nextState[action.listingId]
+        return nextState;
+      default:
+        return state;
+    }
+  };
+  
+  export default listingsReducer;
