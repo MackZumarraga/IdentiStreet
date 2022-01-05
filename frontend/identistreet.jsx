73e557df918545599ffd187d9fe94dba3952cbdf
Mackinjosh <mackzumarraga@gmail.com>
2022-01-04 22:28:17 -0500

    confirmed CRUD for listings from window

--- frontend/identistreet.jsx
+++ frontend/identistreet.jsx
@@ -4,6 +4,7 @@ import configureStore from "./store/store";
 import Root from "./components/root"
 
 import * as SessionActions from "./actions/session_actions"
+import * as ListingApiUtils from "./util/listing_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
@@ -28,6 +29,12 @@ document.addEventListener("DOMContentLoaded", () => {
     window.signup = SessionActions.signup;
     window.login = SessionActions.login;
     window.logout = SessionActions.logout;
+
+    window.fetchListings = ListingApiUtils.fetchListings;
+    window.fetchListing = ListingApiUtils.fetchListing;
+    window.createListing = ListingApiUtils.createListing;
+    window.updateListing = ListingApiUtils.updateListing;
+    window.deleteListing = ListingApiUtils.deleteListing;
 
     ReactDOM.render(<Root store={store} />, root);    
 });
