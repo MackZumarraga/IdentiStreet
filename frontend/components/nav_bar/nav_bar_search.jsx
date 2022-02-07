879eb0a7cef2d3fbf2598ab161b41a00901f087f
Mackinjosh <mackzumarraga@gmail.com>
2022-02-06 19:06:21 -0500

    fixed navbar search logic

--- frontend/components/nav_bar/nav_bar_search.jsx
+++ frontend/components/nav_bar/nav_bar_search.jsx
@@ -18,19 +18,35 @@ class NavBarSearch extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            location: ""
+        }
+
         this.handleSubmit = this.handleSubmit.bind(this); 
         this.update = this.update.bind(this);
     }
 
+    // componentWillUnmount() {
+    //     this.setState({location: ""})
+    // }
+
     handleSubmit(e) {
         e.preventDefault();
+        debugger
+        this.props.updateFilter('location', this.state["location"])
         this.props.fetchListings(getState().ui.filters)
+        this.setState({location: ""})
         this.props.history.push("/search")
     };
 
-    update(filter) {
+    // update(filter) {
+    //     debugger
+    //     return e => this.props.updateFilter(filter, e.currentTarget.value)
+    // }
+
+    update() {
         debugger
-        return e => this.props.updateFilter(filter, e.currentTarget.value)
+        return e => this.setState({location: e.currentTarget.value})
     }
 
     render() {
@@ -38,7 +54,13 @@ class NavBarSearch extends React.Component {
         return (
             <div className="nav-search-container">
                 <form className="search-form" onSubmit={this.handleSubmit}>
-                  <input type="text" placeholder='e.g. neighborhood, borough' className="search-bar-input" onChange={this.update('location')}></input>
+                  <input 
+                    type="text" 
+                    placeholder='e.g. neighborhood, borough' 
+                    className="search-bar-input" 
+                    value={this.state["location"]}
+                    onChange={this.update()}
+                    ></input>
                   <button className="search-button">
                       <span className="search-button-space">Search</span>
                   </button>
